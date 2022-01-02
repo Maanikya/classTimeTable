6ef22c9366fbe4ac2e559922dfe2000108aa968e
Maanikya <maanikya.acharya@gmail.com>
2022-01-02 15:16:09 +0530

    Reaarangement of Files and Folders

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,12 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, url_for
 
 # My Database Connection
 
 local_server=True
 #app=Flask(__name__, template_folder='../frontend', static_url_path='/assets' , static_folder='assets')
 #app=Flask(__name__, template_folder='../backend/assets', static_url_path='/assets' , static_folder='assets')
-app=Flask(__name__, static_url_path='/assets' , static_folder='assets')
+#app=Flask(__name__, static_url_path='/assets' , static_folder='assets')
+app=Flask(__name__)
 app.secret_key="classtimetable"
 
 @app.route("/")
